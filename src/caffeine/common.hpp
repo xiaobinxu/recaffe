c5f831e779fec6a6141711c38f24df5d68b70e27
Xiaobin Xu <xibixu@outlook.com>
2016-04-25 16:39:23 -0700

    update common.hpp and add test

--- src/caffeine/common.hpp
+++ src/caffeine/common.hpp
@@ -5,17 +5,45 @@
 #include <iostream>
 #include <boost/shared_ptr.hpp>
 #include <glog/logging.h>
-#include "driver_types.h"
+
+#include <cublas_v2.h>
+#include <driver_types.h>
 
 namespace caffeine {
-    using boost::shared_ptr;
-    using std::size_t;
-}
 
-static std::ostream nullout(0);
+// We will use the boost shared_ptr instead of the new C++11 one mainly
+// because cuda does not work (at least now) well with C++11 features.
+using boost::shared_ptr;
+using std::size_t;
 
-#define CUDA_CHECK(condition) \
-  CHECK((condition) == cudaSuccess)
+#define CUDA_CHECK(condition) CHECK((condition) == cudaSuccess)
+#define CUBLAS_CHECK(condition) CHECK((condition) == CUBLAS_STATUS_SUCCESS)
+
+// A singleton class to hold common caffeine stuff, such as the handler that
+// caffeine is going to use for cublas.
+class Caffeine
+{
+  public:
+    ~Caffeine();
+    static Caffeine& Get();
+    enum Brew { CPU, GPU };
+
+    // The getters for the variables
+    static cublasHandle_t cublas_handle();
+    static Brew mode();
+    // The setters for the variables
+    static Brew set_mode(Brew mode);
+
+  private:
+    Caffeine();
+    static shared_ptr<Caffeine> singleton_;
+    cublasHandle_t cublas_handle_;
+    Brew mode_;
+};
+
+
+}  // namespace caffeine
+
 
 #endif
 
---
+++ src/caffeine/test_common.cpp
@@ -0,0 +1,24 @@
+#include <cstring>
+#include <cuda_runtime.h>
+
+#include "gtest/gtest.h"
+#include "caffeine/common.hpp"
+
+namespace caffeine {
+
+class CommonTest : public ::testing::Test {};
+
+TEST_F(CommonTest, TestCublasHandler) {
+  int cuda_device_id;
+  CUDA_CHECK(cudaGetDevice(&cuda_device_id));
+  LOG(INFO) << "Cuda device id: " << cuda_device_id;
+  EXPECT_TRUE(Caffeine::cublas_handle());
+}
+
+TEST_F(CommonTest, TestBrewMode) {
+  EXPECT_EQ(Caffeine::mode(), Caffeine::CPU);
+  Caffeine::set_mode(Caffeine::GPU);
+  EXPECT_EQ(Caffeine::mode(), Caffeine::GPU);
+}
+
+}
