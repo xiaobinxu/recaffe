3101e4a419f5256d2b92a2b0e0a054b9f27a1259
Xiaobin Xu <xibixu@outlook.com>
2016-04-25 00:42:05 -0700

    use glog for logging

--- src/caffeine/common.hpp
+++ src/caffeine/common.hpp
@@ -4,6 +4,7 @@
 #include <cstddef>
 #include <iostream>
 #include <boost/shared_ptr.hpp>
+#include <glog/logging.h>
 #include "driver_types.h"
 
 namespace caffeine {
@@ -13,11 +14,8 @@ namespace caffeine {
 
 static std::ostream nullout(0);
 
-#define LOG_IF(condition) \
-    ((condition) == cudaSuccess) ? nullout : std::cout
-
 #define CUDA_CHECK(condition) \
-    LOG_IF(condition) << "Check failed: " #condition " "
+  CHECK((condition) == cudaSuccess)
 
 #endif
 
