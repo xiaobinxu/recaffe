626c82080998f0c26661c6879047eb6fd3ece878
Xiaobin Xu <xibixu@outlook.com>
2016-04-30 15:04:57 -0700

    common: mkl and curand random generator and test, phase

--- src/caffe/common.cpp
+++ src/caffe/common.cpp
@@ -6,9 +6,13 @@ namespace caffe
 shared_ptr<Caffe> Caffe::singleton_;
 
 Caffe::Caffe()
-  : mode_(Caffe::CPU)
+  : mode_(Caffe::CPU), phase_(Caffe::TRAIN), cublas_handle_(NULL),
+  curand_generator_(NULL), vsl_stream_(NULL)
 {
   CUBLAS_CHECK(cublasCreate(&cublas_handle_));
+  //TODO: original caffe code has bug here!
+  CURAND_CHECK(curandCreateGenerator(&curand_generator_, CURAND_RNG_PSEUDO_DEFAULT));
+  CURAND_CHECK(curandSetPseudoRandomGeneratorSeed(curand_generator_, 1701ULL));
   VSL_CHECK(vslNewStream(&vsl_stream_, VSL_BRNG_MT19937, 1701));
 }
 
@@ -16,13 +20,24 @@ Caffe::~Caffe()
 {
   if (cublas_handle_)
     CUBLAS_CHECK(cublasDestroy(cublas_handle_));
+  if (curand_generator_)
+    CURAND_CHECK(curandDestroyGenerator(curand_generator_));
   if (vsl_stream_)
     VSL_CHECK(vslDeleteStream(&vsl_stream_));
 }
 
+void Caffe::set_random_seed(unsigned int seed)
+{
+  CURAND_CHECK(curandDestroyGenerator(Get().curand_generator_));
+  CURAND_CHECK(curandCreateGenerator(&Get().curand_generator_, CURAND_RNG_PSEUDO_DEFAULT));
+  CURAND_CHECK(curandSetPseudoRandomGeneratorSeed(Get().curand_generator_, seed));
+  VSL_CHECK(vslDeleteStream(&Get().vsl_stream_));
+  VSL_CHECK(vslNewStream(&Get().vsl_stream_, VSL_BRNG_MT19937, seed));
+}
+
 Caffe& Caffe::Get()
 {
-  if (!singleton_)
+  if (!singleton_.get())
     singleton_.reset(new Caffe());
   return *singleton_;
 }
@@ -30,6 +45,11 @@ Caffe& Caffe::Get()
 cublasHandle_t Caffe::cublas_handle()
 {
   return Get().cublas_handle_;
+}
+
+curandGenerator_t Caffe::curand_generator()
+{
+  return Get().curand_generator_;
 }
 
 VSLStreamStatePtr Caffe::vsl_stream()
@@ -42,10 +62,19 @@ Caffe::Brew Caffe::mode()
   return Get().mode_;
 }
 
+Caffe::Phase Caffe::phase()
+{
+  return Get().phase_;
+}
+
 void Caffe::set_mode(Caffe::Brew mode)
 {
   Get().mode_ = mode;
 }
 
+void Caffe::set_phase(Caffe::Phase phase)
+{
+  Get().phase_ = phase;
+}
 
 }  // namespace caffe
--- src/caffe/common.hpp
+++ src/caffe/common.hpp
@@ -1,25 +1,52 @@
 #ifndef CAFFE_COMMON_HPP_
 #define CAFFE_COMMON_HPP_
 
-#include <cstddef>
-#include <iostream>
+//#include <cstddef>
+//#include <iostream>
 #include <boost/shared_ptr.hpp>
 #include <glog/logging.h>
 #include <mkl_vsl.h>
 
+#include <cuda.h>
+#include <curand.h>
 #include <cublas_v2.h>
 #include <driver_types.h>
+
+#define CUDA_CHECK(condition) CHECK_EQ((condition), cudaSuccess)
+#define CUBLAS_CHECK(condition) CHECK_EQ((condition), CUBLAS_STATUS_SUCCESS)
+#define CURAND_CHECK(condition) CHECK_EQ((condition), CURAND_STATUS_SUCCESS)
+#define VSL_CHECK(condition) CHECK_EQ((condition), VSL_STATUS_OK)
+
+#define CUDA_POST_KERNEL_CHECK \
+  if (cudaSuccess != cudaPeekAtLastError()) \
+    LOG(FATAL) << "CUDA kernel failed. Error: " \
+               << cudaGetErrorString(cudaPeekAtLastError())
+
+#define DISABLE_COPY_AND_ASSIGN(classname) \
+private: \
+  classname(const classname&); \
+  classname& operator=(const classname&)
+
+#define INSTANTIATE_CLASS(classname) \
+  template class classname<float>; \
+  template class classname<double>
+
+#define NOT_IMPLEMENTED LOG(FATAL) << "Not Implemented Yet"
+
 
 namespace caffe {
 
 // We will use the boost shared_ptr instead of the new C++11 one mainly
 // because cuda does not work (at least now) well with C++11 features.
 using boost::shared_ptr;
-using std::size_t;
+//using std::size_t;
 
-#define CUDA_CHECK(condition) CHECK_EQ((condition), cudaSuccess)
-#define CUBLAS_CHECK(condition) CHECK_EQ((condition), CUBLAS_STATUS_SUCCESS)
-#define VSL_CHECK(condition) CHECK_EQ((condition), VSL_STATUS_OK)
+// For backward compatibility we will just use 512 threads pre block
+const int CAFFE_CUDA_NUM_THREADS = 512;
+
+inline int CAFFE_GET_BLOCKS(const int N) {
+  return (N + CAFFE_CUDA_NUM_THREADS - 1) / CAFFE_CUDA_NUM_THREADS;
+}
 
 // A singleton class to hold common caffe stuff, such as the handler that
 // caffe is going to use for cublas.
@@ -29,20 +56,30 @@ class Caffe
     ~Caffe();
     static Caffe& Get();
     enum Brew { CPU, GPU };
-
+    enum Phase { TRAIN, TEST };
+    
     // The getters for the variables
     static cublasHandle_t cublas_handle();
+    static curandGenerator_t curand_generator();
     static VSLStreamStatePtr vsl_stream();
     static Brew mode();
+    static Phase phase();
+
     // The setters for the variables
     static void set_mode(Brew mode);
+    static void set_phase(Phase phase);
+    static void set_random_seed(unsigned int seed);
 
   private:
-    Caffe();
+    Caffe(); // private ctor to avoid duplicate instantiation
     static shared_ptr<Caffe> singleton_;
-    cublasHandle_t cublas_handle_;
-    VSLStreamStatePtr vsl_stream_;
     Brew mode_;
+    Phase phase_;
+    cublasHandle_t cublas_handle_;
+    curandGenerator_t curand_generator_;
+    VSLStreamStatePtr vsl_stream_;
+
+    DISABLE_COPY_AND_ASSIGN(Caffe);
 };
 
 
